178268744179d5a0e87e0b147b1b5e5e4d6dc15f
Steven Carey <steven.carey@skyscanner.net>
2018-01-25 19:36:15 +0000

    Add requirements and start tests

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+
+# class CreateTempFile(object):
+
+#     def __init__(self, tempdir):
+#         self.tempdir
+
+#     def __enter__(self):
+#         with 
+
+#     def __exit__(self, err_type, value, traceback):
+#         pass
